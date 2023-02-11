294f93611da60fac6f035b27508ded7a630d7a7f
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2023-02-11 10:23:09 +0700

    them vao gio hang

--- details.php
+++ details.php
@@ -8,6 +8,10 @@ if (!isset($_GET['productid']) || $_GET['productid'] == NULL) {
 } else {
 	$id = $_GET['productid'];
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+	$quantity = $_POST['quantity'];
+	$Addcart = $ct->addToCart($quantity,$id);
+}
 ?>
 <div class="main">
 	<div class="content">
@@ -26,48 +30,54 @@ if (!isset($_GET['productid']) || $_GET['productid'] == NULL) {
 							<h2><?php echo $result_details['productName'] ?></h2>
 							<p><?php echo $fm->textShorten($result_details['description']) ?></p>
 							<div class="price">
-								<p>Price: <span><?php echo $result_details['price']." VNĐ" ?></span></p>
+								<p>Price: <span><?php echo $result_details['price'] . " VNĐ" ?></span></p>
 								<p>Category: <span><?php echo $result_details['catName'] ?></span></p>
 								<p>Brand:<span><?php echo $result_details['brandName'] ?></span></p>
 							</div>
 							<div class="add-cart">
-								<form action="cart.php" method="post">
-									<input type="number" class="buyfield" name="" value="1" />
+								<form action="" method="post">
+									<input type="number" class="buyfield" name="quantity" value="1" min="1" />
 									<input type="submit" class="buysubmit" name="submit" value="Buy Now" />
 								</form>
+									<?php
+									if(isset($Addcart)){
+										echo "<hr>";
+										echo "<span style = 'color: blue'>Sản phẩm đã được thêm thành công</span>";
+									}
+									?>
 							</div>
 						</div>
 						<div class="product-desc">
 							<h2>Product Details</h2>
-							<p><?php echo $result_details['description']." VNĐ" ?></p>
-							
+							<p><?php echo $result_details['description'] . " VNĐ" ?></p>
+
 						</div>
 
 					</div>
-				<?php
+			<?php
 				}
 			}
-				?>
+			?>
 
 
-					<div class="rightsidebar span_3_of_1">
-						<h2>CATEGORIES</h2>
-						<ul>
-							<li><a href="productbycat.php">Mobile Phones</a></li>
-							<li><a href="productbycat.php">Desktop</a></li>
-							<li><a href="productbycat.php">Laptop</a></li>
-							<li><a href="productbycat.php">Accessories</a></li>
-							<li><a href="productbycat.php#">Software</a></li>
-							<li><a href="productbycat.php">Sports & Fitness</a></li>
-							<li><a href="productbycat.php">Footwear</a></li>
-							<li><a href="productbycat.php">Jewellery</a></li>
-							<li><a href="productbycat.php">Clothing</a></li>
-							<li><a href="productbycat.php">Home Decor & Kitchen</a></li>
-							<li><a href="productbycat.php">Beauty & Healthcare</a></li>
-							<li><a href="productbycat.php">Toys, Kids & Babies</a></li>
-						</ul>
+			<!-- <div class="rightsidebar span_3_of_1">
+				<h2>CATEGORIES</h2>
+				<ul>
+					<li><a href="productbycat.php">Mobile Phones</a></li>
+					<li><a href="productbycat.php">Desktop</a></li>
+					<li><a href="productbycat.php">Laptop</a></li>
+					<li><a href="productbycat.php">Accessories</a></li>
+					<li><a href="productbycat.php#">Software</a></li>
+					<li><a href="productbycat.php">Sports & Fitness</a></li>
+					<li><a href="productbycat.php">Footwear</a></li>
+					<li><a href="productbycat.php">Jewellery</a></li>
+					<li><a href="productbycat.php">Clothing</a></li>
+					<li><a href="productbycat.php">Home Decor & Kitchen</a></li>
+					<li><a href="productbycat.php">Beauty & Healthcare</a></li>
+					<li><a href="productbycat.php">Toys, Kids & Babies</a></li>
+				</ul>
 
-					</div>
+			</div> -->
 
 		</div>
 	</div>
