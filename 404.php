91dd450af3d0ea92f40804a50ed4d2d902e1bd07
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2023-02-15 09:23:24 +0700

    update link

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin/css/404.css">
+    <link rel="stylesheet" href="css/404.css">
     <title>Document</title>
 </head>
 <body>
@@ -101,7 +101,7 @@
     <div class="text">
         <h1>404 Error</h1>  
       <h2>Couldn't launch :(</h2>
-         <h3>Page Not Found - lets take you <a href="https://twitter.com/namratapdr">BACK</a></h3> 
+         <h3>Page Not Found - lets take you <a href="http://localhost/doan/index.php">BACK</a></h3> 
     </div>
 </body>
 
