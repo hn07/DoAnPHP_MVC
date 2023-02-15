5194da9f9aad37fd3fbcd37ab7ccc0522c92799c
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2023-02-15 09:29:32 +0700

    update

--- classes/product.php
+++ classes/product.php
@@ -1,8 +1,8 @@
 <!-- dam nhiem san pham -->
 <?php
 $filepath = realpath(dirname(__FILE__));
-include_once $filepath.('/../lib/database.php');
-include_once $filepath.('/../helpers/format.php');
+include_once $filepath . ('/../lib/database.php');
+include_once $filepath . ('/../helpers/format.php');
 
 ?>
 
@@ -28,17 +28,17 @@ class product
         $proType = mysqli_real_escape_string($this->db->link, $data['type']);
 
         // kiem tra va lay hinh anh cho vao folder upload
-        $permited = array('jpg','png','jpeg','gif');
+        $permited = array('jpg', 'png', 'jpeg', 'gif');
         $file_name = $_FILES['image']['name'];
         $file_size = $_FILES['image']['size'];
         $file_temp = $_FILES['image']['tmp_name'];
 
         $div = explode('.', $file_name);
         $file_ext = strtolower(end($div));
-        $unique_image = substr(md5(time()), 0, 10).'.'. $file_ext;
-        $uploadde_image = 'uploads/'.$unique_image;
+        $unique_image = substr(md5(time()), 0, 10) . '.' . $file_ext;
+        $uploadde_image = 'uploads/' . $unique_image;
 
-        if ($proName == "" ||$proBrand == "" ||$prodCategory == "" ||$proDesc == "" ||$proPrice == "" ||$proType == ""|| $file_name == "") {
+        if ($proName == "" || $proBrand == "" || $prodCategory == "" || $proDesc == "" || $proPrice == "" || $proType == "" || $file_name == "") {
             $alert = "Các trường không được rỗng";
             return $alert;
         } else {
@@ -66,7 +66,7 @@ class product
         INNER JOIN tbl_brand ON tbl_product.brandId = tbl_brand.brandId 
 
         ORDER BY tbl_product.productId desc";
-        
+
         $result = $this->db->select($query);
         return $result;
     }
@@ -77,7 +77,7 @@ class product
         return $result;
     }
 
-    public function update_product($data,$files, $id)
+    public function update_product($data, $files, $id)
     {
         $proName = mysqli_real_escape_string($this->db->link, $data['productName']);
         $proBrand = mysqli_real_escape_string($this->db->link, $data['brand']);
@@ -87,72 +87,55 @@ class product
         $proType = mysqli_real_escape_string($this->db->link, $data['type']);
 
         // kiem tra va lay hinh anh cho vao folder upload
-        $permited = array('jpg','png','jpeg','gif');
+        $permited = array('jpg', 'png', 'jpeg', 'gif');
         $file_name = $_FILES['image']['name'];
         $file_size = $_FILES['image']['size'];
         $file_temp = $_FILES['image']['tmp_name'];
 
         $div = explode('.', $file_name);
         $file_ext = strtolower(end($div));
-        $unique_image = substr(md5(time()), 0, 10).'.'. $file_ext;
-        $uploadde_image = 'uploads/'.$unique_image;
+        $unique_image = substr(md5(time()), 0, 10) . '.' . $file_ext;
+        $uploadde_image = 'uploads/' . $unique_image;
 
-        if ($proName == "" ||$proBrand == "" ||$prodCategory == "" ||$proDesc == "" ||$proPrice == "" ||$proType == "") {
+        if ($proName == "" || $proBrand == "" || $prodCategory == "" || $proDesc == "" || $proPrice == "" || $proType == "") {
             $alert = "Các trường không được rỗng";
             return $alert;
-        }else{
-            if(!empty($file_name)){
+        } else {
+            if (!empty($file_name)) {
                 // neu nguoi dung chon anh
-                if($file_size > 10.240){
+                if ($file_size > 10.240) {
                     $alert = "<span class ='succsess'>Tập tin hình ảnh nên có kích thước dưới 2MB </span>";
                     return $alert;
-                }elseif(in_array($file_ext,$permited) === false){
-                    $alert = "<span class ='succsess'>Chỉ được up load các file có phần mở rộng là: ".implode(', ',$permited)."</span>";
+                } elseif (in_array($file_ext, $permited) === false) {
+                    $alert = "<span class ='succsess'>Chỉ được up load các file có phần mở rộng là: " . implode(', ', $permited) . "</span>";
                     return $alert;
                 }
-            $query = "UPDATE tbl_product SET
-            productName = '$proName', 
-            catid = '$prodCategory',
-            brandid = '$proBrand', 
-            description = '$proDesc', 
-            type = '$proType', 
-            price = '$proPrice', 
-            image = '$unique_image'
-
-            WHERE productId = '$id'";
-            }
-            else{
+                $query = "UPDATE tbl_product SET productName = '$proName',catid = '$prodCategory',brandid = '$proBrand', description = '$proDesc', type = '$proType', price = '$proPrice',image = '$unique_image' WHERE productId = '$id'";
+            } else {
                 // neu nguoi dung kh chon anh
 
-            $query = "UPDATE tbl_product SET
-            productName = '$proName', 
-            catid = '$prodCategory',
-            brandid = '$proBrand', 
-            description = '$proDesc', 
-            type = '$proType', 
+                $query = "UPDATE tbl_product SET productName = '$proName', catid = '$prodCategory', brandid = '$proBrand', description = '$proDesc', type = '$proType', 
             price = '$proPrice'
 
             WHERE productId = '$id'";
             }
-
         }
-        
-            $result = $this->db->update($query);
+        $result = $this->db->update($query);
 
-            if ($result != false) {
-                $alert = "<span class ='succsess'> Update   Successfully</span>";
-                return $alert;
-            } else {
-                $alert = "<span class ='error'>Update NOT Success</span>";
-                return $alert;
-            }
-        
+        if ($result != false) {
+            $alert = "<span class ='succsess'> Update   Successfully</span>";
+            return $alert;
+        } else {
+            $alert = "<span class ='error'>Update NOT Success</span>";
+            return $alert;
+        }
     }
 
-    public function del_product($id){
+    public function del_product($id)
+    {
         $query = "DELETE FROM tbl_product WHERE productId = '$id'";
         $result = $this->db->delete($query);
-        if($result){
+        if ($result) {
             $alert = "<span class ='succsess'> Delete Product Successfully</span>";
             return $alert;
         } else {
@@ -168,23 +151,24 @@ class product
         $query = "SELECT * FROM tbl_product WHERE type = 1";
         $result = $this->db->select($query);
         return $result;
-    } public function getproducts_new()
+    }
+    public function getproducts_new()
     {
         $query = "SELECT * FROM tbl_product WHERE type = 0";
         $result = $this->db->select($query);
         return $result;
     }
-    public function getproducts_detail($id){
+    public function getproducts_detail($id)
+    {
         $query = "SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName
 
         FROM tbl_product INNER JOIN tbl_category ON  tbl_product.catId = tbl_category.catId 
 
         INNER JOIN tbl_brand ON tbl_product.brandId = tbl_brand.brandId 
         WHERE tbl_product.productId = '$id' ";
-        
+
         $result = $this->db->select($query);
         return $result;
     }
-    
 }
 ?>
