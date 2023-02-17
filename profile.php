a5329e6ee5926450d2f3e57ea222691461ca9f95
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2023-02-17 16:09:37 +0700

    create profile

---
+++ profile.php
@@ -0,0 +1,74 @@
+<?php
+include 'inc/header.php';
+// include 'inc/slider.php';
+?>
+<?php
+$login_check = Session::get('login_customer');
+if ($login_check == false) {
+    header('Location:login.php');
+}
+?>
+
+<div class="main">
+    <div class="content">
+        <div class="section group">
+            <table class="tblone">
+                <?php
+                $id = Session::get('id_customer');
+                $get_customer_profile = $cs->showCustomerProfile($id);
+                if ($get_customer_profile) {
+                    while ($result = $get_customer_profile->fetch_assoc()) {
+
+                ?>
+                        <tr>
+                            <td colspan="3"><h2 style="font-size: 28px;" href="editprofile.php">My Profile</h2></td>
+                            
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Tên</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['name'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Địa chỉ </td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['address'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Thành phố</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['city'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Số điện thoại</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['phone'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Email</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['email'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Địa chỉ bưu chính</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['address_zipcode'] ?></td>
+                        </tr>
+                        <tr>
+                            <td style="display: flex; justify-content: left;">>Mã bưu chính</td>
+                            <td>:</td>
+                            <td style="display: flex; justify-content: left;"><?php echo $result['zipcode'] ?></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3"><a href="editprofile.php">Update Profile</a></td>
+                        </tr>
+                <?php
+                    }
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+    <?php
+    include 'inc/footer.php';
+    ?>
